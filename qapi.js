6a25335af5d5045e31e6b4479d5dd7e0f7f35c50
Aadarshkt <72285744+Aadarshkt@users.noreply.github.com>
2020-12-06 10:09:18 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fcc5db5e563f3001ecb70a9",
+				"5fcc5db5e563f3001ecb70ab"
+			]
+		},
+		"5fcc5db5e563f3001ecb70a9": {
+			"id": "5fcc5db5e563f3001ecb70a9",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fcc5db5e563f3001ecb70ab": {
+			"id": "5fcc5db5e563f3001ecb70ab",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
